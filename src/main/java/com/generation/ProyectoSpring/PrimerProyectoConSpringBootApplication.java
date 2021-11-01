9188eba57ff93b02662c5fd4428d7ef9e5c6211c
Abigail Romero <romerot.abigail@gmail.com>
2021-10-31 19:38:41 -0600

    Primer proyecto con Spring en Java

---
+++ src/main/java/com/generation/ProyectoSpring/PrimerProyectoConSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.generation.ProyectoSpring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrimerProyectoConSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrimerProyectoConSpringBootApplication.class, args);
+	}
+
+}
